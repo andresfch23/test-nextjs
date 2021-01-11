5022bb9fbe45b3b621d9c314def6ea0147d7a7a9
Andres Chavez <Andres.F.Chavez.-ND@abc.com>
2021-01-11 14:36:46 -0500

    Starting app

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -22,8 +22,8 @@ const ProductPage = () => {
   return (
     <section>
       <h1>{product?.name}</h1>
-      <p>{product?.attributes?.description}</p>
-      <img src={product?.image}></img>
+      {/* <p>{product?.attributes?.description}</p>
+      <img src={product?.image}></img> */}
     </section>
   )
 }
